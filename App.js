3bc46f837a32a45d9d54935f98572f7b0e4b0964
arbaz0007 <arbazk.ak007@gmail.com>
2023-07-30 19:55:13 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
+//Form validation 
+import * as Yup from 'yup'
+const PasswordSchema = Yup.object().shape({
+passwordLength : Yup.number()
+.min(4,'should be 4 char min ')
+.max(16,'should be 16 char max')
+.required('length is required')   // it define that length is required 
+})
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>App</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({})
